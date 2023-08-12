2dd287ed9088245784f62d7a4297180eda21f2f1
pdy1207 <pdyme1207@gmail.com>
2023-08-12 19:33:40 +0900

    🫵 feat: [String] 문자 찾기 [23.08.12] - 문자열 스캔 / 받은 문자를 올려서 비교 - 1. 전통적인 for 방식 - 2. forEach 방식 tocharArray String을 문자 배열로 만들어준다

---
+++ Java Infrastructure/1. String/StringFound.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Main {
+    public int solution(String str, char t){
+        int answer =0; 
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        /* 
+         * 받은 두개의 문자를 올려서 비교하는 식
+         *  System.out.println(str + " " + t); 
+         */     
+
+        /* 
+            1. 전통적인 for 방식
+
+            for(int i =0; i<str.length(); i++){
+                if(str.charAt(i) == t){
+                    answer ++;
+                }
+            }             
+        */
+
+        for (char x : str.toCharArray()){  
+            // toCharArray String을 문자 배열로 만들어준다.
+            if(x == t){
+                    answer ++;
+            }
+        }
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str= kb.next(); // 문자열 스캔
+        char c = kb.next().charAt(0); // 문자 한개 스캔
+
+        System.out.println(T.solution(str,c));
+    }
+}
+
+
--- Java Infrastructure/Hello.java
+++ Java Infrastructure/Hello.java
@@ -1,5 +1,21 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World JAVA of vscode ");
+import java.util.Scanner;
+
+class Main {
+    public int solution(String str, char t){
+        int answer =0; 
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str= kb.next(); // 문자열 스캔
+        char c = kb.next().charAt(0); // 문자 한개 스캔
+
+        System.out.println(T.solution(str,c));
     }
 }
+
+
