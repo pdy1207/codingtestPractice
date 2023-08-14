9c9e309405f8ffada31de9021ffde0a582b10190
pdy1207 <pdyme1207@gmail.com>
2023-08-14 21:55:50 +0900

    🫵 feat: 특정 문자 뒤집기(toCharArray()) [23.08.14] - Amm.. NoJam... - 해당 개념 다시 숙지하기 지금은 뇌용량 과다😵‍💫

---
+++ Java Infrastructure/1. String/5. 특정 문자 뒤집기 (toCharArray())/WordFilp2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class WordFilp2 {
+    public String solution(String str){
+        String answer;
+        char[] s = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+        while(lt<rt){
+            if(!Character.isAlphabetic(s[lt])){
+                lt++;
+            } else if (!Character.isAlphabetic(s[lt])){
+                rt--; 
+            } else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = String.valueOf(s);
+        return answer;
+    }
+
+    public static void main(String[] args){
+        WordFilp2 T = new WordFilp2();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
