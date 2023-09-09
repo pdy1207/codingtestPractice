9a6102d9caeb3e428faed3c382589590b6b31986
pdy1207 <pdyme1207@gmail.com>
2023-09-09 13:49:01 +0900

    🫵 feat: 두 배열 합치기 (two pointers algorithm) [23.09.09] ++ 이 앞에 있으면 전임증감 연산자 ++ 이 뒤에 있으면 후임증감 연산자 즉, 앞에 있으면 먼저 증가하고 a를 추가

---
+++ Java Infrastructure/3. Two pointers, Sliding window[효율성 O(n^2) O(n)]/1. 두 배열 합치기(two pointers algorithm)/CombineTwoArrays.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class CombineTwoArrays {
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while(p1 < n && p2 < m){
+            if(a[p1] < b[p2]){
+                answer.add(a[p1++]);            
+                // ++이 앞에 있다가 전이증감 연산자 뒤에는 후임증감연산자 
+                // 즉, 앞에있으면 먼저 증가하고 a를 추가
+                
+            } else {
+                answer.add(b[p2++]);
+            }
+        }
+        while(p1 < n) {
+            answer.add(a[p1++]);
+        }
+        while(p2 < m) {
+            answer.add(b[p2++]);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        CombineTwoArrays T = new CombineTwoArrays();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++){
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for(int i = 0; i < m; i++){
+            b[i] = kb.nextInt();
+        }
+        for(int x : T.solution(n, m, a, b)) System.out.print(x + " ");
+
+    }
+}
